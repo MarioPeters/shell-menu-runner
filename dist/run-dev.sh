1900d2acb2f768176477ac42acf47235cc92c5ee
Mario Peters <mario.peters@ionos.com>
2026-07-10 17:43:31 +0200

    ui: compact layout, ghost-text fix, adaptive footer

--- dist/run-dev.sh
+++ dist/run-dev.sh
@@ -17,7 +17,7 @@ fi
 #  Lizenz: MIT
 # ==============================================================================
 
-readonly VERSION="2.0.1"
+readonly VERSION="2.0.2"
 readonly LOCAL_CONFIG=".tasks"
 readonly GLOBAL_CONFIG="$HOME/.tasks"
 readonly LOCAL_SETTINGS=".runrc"
